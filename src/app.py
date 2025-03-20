fd86b2a3713c327030028b85eb042ebf752bef95
Ekaitz Martin <ekamartinc2003@example.com>
2025-03-20 12:15:14 +0100

    DELETE request + verificar_user() functionccreated and  added to the different requests

--- src/app.py
+++ src/app.py
@@ -28,10 +28,10 @@ def listar_usuarios():
                        "follower_count": dato[6],
                        "created_at": dato[7]}
             usuarios.append(usuario)
-        return jsonify({"Usuarios": usuarios})
+        return jsonify({"Users": usuarios})
         
     except Exception as e:
-        return jsonify({"Mensaje":"Error"}), 500
+        return jsonify({"Error": str(e)}), 400
 #MOSTRAR TODOS LOS SEGUIDORES 
 @app.route("/followers", methods = ["GET"])
 def listar_follows():
@@ -46,9 +46,9 @@ def listar_follows():
             follows.append(follow)
         return jsonify({"Follows": follows})
     except Exception as e:
-        return jsonify({"Mensaje": "Error"}), 500
+        return jsonify({"Error": str(e)}), 400
 
-#MOSTRAR TODOS LOS USUARIOS CON UN DETERMINADO ID
+#MOSTRAR USUARIO CON UN DETERMINADO ID
 @app.route("/users/<user_id>", methods = ["GET"])
 def listar_usuario(user_id):
     try:
@@ -56,7 +56,7 @@ def listar_usuario(user_id):
         query = f"""SELECT * FROM users WHERE user_id = {user_id} """
         cursor.execute(query)
         dato = cursor.fetchone()
-        if dato != None:
+        if verificar_user(user_id) == True:
             usuario = {"user_id": dato[0],
                         "user_handle": dato[1],
                         "email_address": dato[2],
@@ -65,9 +65,10 @@ def listar_usuario(user_id):
                         "phone_number": dato[5],
                         "follower_count": dato[6],
                         "created_at": dato[7]}
-        return jsonify({"Usuario": usuario})
+            return jsonify({"User": usuario})
+        return jsonify({"Error": f"User with user_id {user_id} does not exist"})
     except Exception as e:
-        return jsonify({"Mensaje": "Error"}), 500
+        return jsonify({"Error": str(e)}), 400
 
 #MOSTRAR TODOS LOS FOLLOWERS CON UN DETERMINADO FOLLOWING_ID 
 @app.route("/followers/<following_id>", methods = ["GET"])
@@ -77,45 +78,70 @@ def listar_follow(following_id):
         query = f"""SELECT * FROM followers WHERE following_id = {following_id}"""
         cursor.execute(query)
         datos = cursor.fetchall()#aquí pongo fetchall porque hay más de un follower_id que sigue al mismo following_id, por lo que quiero que salgan todos
-        if datos != None:
+        if verificar_user(following_id) == True:
+            conexion.connection.commit()
             return jsonify({"follower_id": datos[0], "following_id": datos[1]})
+        return jsonify({"Error": f"User with following_id {following_id} does not exist"}) 
     except Exception as e:
-        return jsonify({"Mensaje": "Error"}), 500
+        return jsonify({"Error": str(e)}), 400
     
 #AÑADIR USERS
 @app.route("/users", methods = ["POST"])
 def añadir_user():
     try:
         cursor = conexion.connection.cursor()
-        query = f"""INSERT INTO users (user_id, user_handle, email_address, first_name, last_name, phone_number, follower_count, created_at)
-        VALUES ({request.json["user_id"]}, {request.json["user_handle"]}, {request.json["email_address"]}, {request.json["first_name"]}, 
-        {request.json["last_name"]}, {request.json["phone_number"]}, {request.json["follower_count"]}, {request.json["created_at"]})"""
-        cursor.execute(query)
+        query = """INSERT INTO users (user_handle, email_address, first_name, last_name, phone_number, follower_count)
+        VALUES (%s, %s, %s, %s, %s, %s)"""
+        data = [request.json["user_handle"], request.json["email_address"], request.json["first_name"], 
+        request.json["last_name"], request.json["phone_number"], request.json["follower_count"]]
+        cursor.execute(query, data)
         conexion.connection.commit()
-        return jsonify({"Mensaje": "Usuario añadido correctamente"})
+        return jsonify({"Message": "User successfully added"})
     except Exception as e:
-        raise e
-
-    
+        return jsonify({"Error": str(e)}), 400    
 
 #AÑADIR FOLLOWERS    
 @app.route("/followers", methods = ["POST"])
 def añadir_follow():
     try:
         cursor= conexion.connection.cursor()
-        query = f"""INSERT INTO followers (follower_id, following_id)
-        VALUES
-        ({request.json["follower_id"]}, {request.json["following_id"]})"""
-        cursor.execute(query)
+        query = """INSERT INTO followers (follower_id, following_id)
+        VALUES (%s, %s)"""
+        datos = [request.json["follower_id"], request.json["following_id"]]
+        cursor.execute(query,datos)
         conexion.connection.commit()
-        return jsonify({"Mensaje":"Followers registrados"})
+        return jsonify({"Message":"Follow added"})
     except Exception as e:
-        return jsonify({"Mensaje": "Error"}), 500
+        return jsonify({"Error": str(e)}), 400
     
+#ELIMINAR USER
+@app.route("/users/<user_id>", methods = ["DELETE"])
+def delete_users(user_id):
+    try: 
+        cursor = conexion.connection.cursor()
+        query1 = """DELETE FROM users WHERE user_id = (%s)"""
+        usuario = user_id
+        if verificar_user(usuario) == True:
+            cursor.execute(query1, usuario)
+            conexion.connection.commit()
+            return jsonify({"Message": "User successfully deleted"})
+        return jsonify({"Error": f"User with user_id {usuario} does not exist"})
+    except Exception as e:
+        return jsonify({"Error": str(e)})
+
+
 
 
 def pagina_no_encontrada(error):
-    return jsonify({"Error":"La página que intentas buscar no existe..."})
+    return jsonify({"Error":"Page not found"})
+
+def verificar_user(user_id):
+    cursor = conexion.connection.cursor()
+    query = """SELECT * FROM users"""
+    cursor.execute(query)
+    if user_id in query:
+        return True
+    return False
 
 
 if __name__ == "__main__":
