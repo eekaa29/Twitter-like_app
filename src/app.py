a1e769bdd66b264f79729fb4191f0c26244adf85
Ekaitz Martin <ekamartinc2003@example.com>
2025-03-20 13:56:15 +0100

    PUT request added + verificar_user function fixed

--- src/app.py
+++ src/app.py
@@ -2,12 +2,10 @@ from flask import Flask, jsonify, json, request
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_mysqldb import MySQL
-import logging
+
 app = Flask(__name__)
 
 conexion = MySQL(app)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 #MOSTRAR TODOS LOS USUARIOS 
 @app.route("/users", methods = ["GET"])
@@ -53,10 +51,10 @@ def listar_follows():
 def listar_usuario(user_id):
     try:
         cursor = conexion.connection.cursor()
-        query = f"""SELECT * FROM users WHERE user_id = {user_id} """
-        cursor.execute(query)
-        dato = cursor.fetchone()
-        if verificar_user(user_id) == True:
+        query = """SELECT * FROM users WHERE user_id = %s """ #El $s es evitar usar el .format, evitando fallos de sintaxis y riesgo de inyecciones SQL 
+        if verificar_user(user_id):
+            cursor.execute(query, user_id)
+            dato = cursor.fetchone()
             usuario = {"user_id": dato[0],
                         "user_handle": dato[1],
                         "email_address": dato[2],
@@ -66,22 +64,23 @@ def listar_usuario(user_id):
                         "follower_count": dato[6],
                         "created_at": dato[7]}
             return jsonify({"User": usuario})
-        return jsonify({"Error": f"User with user_id {user_id} does not exist"})
+        return jsonify({"Error": f"User with user_id {user_id} does not exists"})
     except Exception as e:
         return jsonify({"Error": str(e)}), 400
 
-#MOSTRAR TODOS LOS FOLLOWERS CON UN DETERMINADO FOLLOWING_ID 
+#MOSTRAR TODOS LOS FOLLOWERS DE UN DETERMINADO USER 
 @app.route("/followers/<following_id>", methods = ["GET"])
 def listar_follow(following_id):
     try:
         cursor = conexion.connection.cursor()
-        query = f"""SELECT * FROM followers WHERE following_id = {following_id}"""
-        cursor.execute(query)
-        datos = cursor.fetchall()#aquí pongo fetchall porque hay más de un follower_id que sigue al mismo following_id, por lo que quiero que salgan todos
-        if verificar_user(following_id) == True:
+        query = """SELECT * FROM followers WHERE following_id = %s"""
+        if verificar_user(following_id):
+            cursor.execute(query, following_id)
+            datos = cursor.fetchall()#aquí pongo fetchall() porque hay más de un follower_id que sigue al mismo following_id, por lo que quiero que salgan todos
+            follower_list = [follower[0] for follower in datos]# Datos es un lisata de tuplas;meto en la lista el primer valor de cada tupla
             conexion.connection.commit()
-            return jsonify({"follower_id": datos[0], "following_id": datos[1]})
-        return jsonify({"Error": f"User with following_id {following_id} does not exist"}) 
+            return jsonify({"following_id": following_id, "followers": follower_list})
+        return jsonify({"Error": f"User with following_id {following_id} does not exists"}) 
     except Exception as e:
         return jsonify({"Error": str(e)}), 400
     
@@ -125,23 +124,29 @@ def delete_users(user_id):
             cursor.execute(query1, usuario)
             conexion.connection.commit()
             return jsonify({"Message": "User successfully deleted"})
-        return jsonify({"Error": f"User with user_id {usuario} does not exist"})
+        return jsonify({"Error": f"User with user_id {usuario} does not exists"})
     except Exception as e:
         return jsonify({"Error": str(e)})
 
-
-
+#MODIFICAR USERS
+@app.route("/users/<user_id>", methods = ["PUT"])
+def modify_user(user_id):
+    cursor = conexion.connection.cursor()
+    query = """UPDATE users SET first_name = %s WHERE user_id = %s"""
+    if verificar_user(user_id):
+        cursor.execute(query, (request.json["first_name"], user_id))
+        conexion.connection.commit()
+        return jsonify({"Message": f"User with user_id {user_id} has been updated "})
+    return jsonify({"Message": f"User with user_id {user_id} does not exists"})
 
 def pagina_no_encontrada(error):
     return jsonify({"Error":"Page not found"})
 
 def verificar_user(user_id):
     cursor = conexion.connection.cursor()
-    query = """SELECT * FROM users"""
-    cursor.execute(query)
-    if user_id in query:
-        return True
-    return False
+    query = """SELECT * FROM users WHERE user_id = %s"""
+    cursor.execute(query, user_id)
+    return cursor.fetchone() is not None #Devuleve True si el usuario existe y False si no existe(porque sería == None)
 
 
 if __name__ == "__main__":
